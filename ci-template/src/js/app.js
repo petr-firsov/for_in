ed226df34d75b38acb45c54ea840b3fdfeb6f339
Пётр Фирсов <peterfirsoff@gmail.com>
2024-06-26 11:00:14 +0300

    26 июня

--- ci-template/src/js/app.js
+++ ci-template/src/js/app.js
@@ -2,14 +2,12 @@ export const hero = {name: 'мечник', health: 10, level: 2, attack: 80, def
 export const mainArgs = ["name", "level"];
 
 export function orderByProps(object, args) {
-    let a = Object.keys(object);
-    let b = a.filter(element => {
+    let keys = Object.keys(object).filter(element => {
         if (!args.includes(element)) {
             return element;
         }
-    });
-    let c = b.sort();
-    let properties = args.concat(c);
+    }).sort();
+    let properties = args.concat(keys);
 
     const heroTraits = []
     for (let i = 0; i < properties.length; i++) {
